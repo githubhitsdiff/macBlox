2c54239b71839d0c31ff88006d5497b59d93686c
2HighOnGray <161499940+githubhitsdiff@users.noreply.github.com>
2024-11-24 16:18:49 +0800

    Update vite-env.d.ts

--- frontend/src/types/vite-env.d.ts
+++ frontend/src/types/vite-env.d.ts
@@ -1,7 +1,15 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
-declare module '*.svelte';
+
+// Declares modules for .svelte files to be treated as Svelte components
+declare module '*.svelte' {
+  import { SvelteComponent } from 'svelte';
+  const component: SvelteComponent;
+  export default component;
+}
+
+// Declares modules for .icns files (macOS icon files) as strings
 declare module '*.icns' {
-	const src: string;
-	export default src;
+  const src: string;
+  export default src;
 }
