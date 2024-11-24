1066c7a502c95362ce60dd577891547e64dbe646
2HighOnGray <161499940+githubhitsdiff@users.noreply.github.com>
2024-11-24 16:10:07 +0800

    Update build.config.ts

--- build.config.ts
+++ build.config.ts
@@ -2,23 +2,25 @@ import { resolve as path } from 'node:path';
 import type { Config } from './scripts/build/ts/config-types';
 
 const BuildConfig: Config = {
-	devPort: 5174,
-	projectPath: path('./frontend/dist'),
-	outDir: path('./dist'),
-	appName: 'AppleBlox',
-	description: 'A MacOS Roblox Launcher',
-	appBundleName: 'AppleBlox',
-	mac: {
-		architecture: ['universal', 'arm64', 'x64'],
-		appIcon: path('./scripts/build/assets/mac.icns'),
-		minimumOS: '10.13.0',
-	},
-	// May sometimes test some things
-	// win: {
-	//     architecture: ["x64"],
-	//     appIcon: path("./frontend/src/assets/favicon.png"),
-	//     embedResources: false
-	// }
+  devPort: 5174,
+  projectPath: path('./frontend/dist'),
+  outDir: path('./dist'),
+  appName: 'AppleBlox',
+  description: 'A MacOS Roblox Launcher',
+  appBundleName: 'AppleBlox',
+
+  mac: {
+    architecture: ['universal', 'arm64', 'x64'],
+    appIcon: path('./scripts/build/assets/mac.icns'),
+    minimumOS: '10.13.0',
+  },
+
+  // Uncomment the section below if you plan to test for Windows
+  // win: {
+  //   architecture: ['x64'],
+  //   appIcon: path('./frontend/src/assets/favicon.png'),
+  //   embedResources: false,
+  // }
 };
 
 export default BuildConfig;
